38ae0b1b7d27c39d9b1b44fdc641636088648b65
Arrizal28 <2010631170088@student.unsika.ac.id>
2024-10-10 21:19:49 +0700

    feat/complete_product_section

--- app/src/main/java/com/xyvona/flavorville/ui/screen/ProductDetailsScreen.kt
+++ app/src/main/java/com/xyvona/flavorville/ui/screen/ProductDetailsScreen.kt
@@ -1,25 +1,108 @@
 package com.xyvona.flavorville.ui.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.xyvona.flavorville.data.OrderData
+import com.xyvona.flavorville.data.OrderState
+import com.xyvona.flavorville.data.ProductDescriptionData
+import com.xyvona.flavorville.data.ProductFlavorState
+import com.xyvona.flavorville.data.ProductFlavorsData
+import com.xyvona.flavorville.data.ProductNutritionData
+import com.xyvona.flavorville.data.ProductNutritionState
 import com.xyvona.flavorville.data.ProductPreviewState
+import com.xyvona.flavorville.ui.screen.components.FlavorSection
+import com.xyvona.flavorville.ui.screen.components.OrderActionBar
+import com.xyvona.flavorville.ui.screen.components.ProductDescriptionSection
+import com.xyvona.flavorville.ui.screen.components.ProductNutritionSection
 import com.xyvona.flavorville.ui.screen.components.ProductPreviewSection
 
 @Composable
-fun ProductDetailsScreen (
+fun ProductDetailsScreen(
     modifier: Modifier = Modifier,
     productPreviewState: ProductPreviewState = ProductPreviewState(),
+    productFlavors: List<ProductFlavorState> = ProductFlavorsData,
+    productNutritionState: ProductNutritionState = ProductNutritionData,
+    productDescription: String = ProductDescriptionData,
+    orderState: OrderState = OrderData,
+    onAddItemClicked: () -> Unit = {},
+    onRemoveItemClicked: () -> Unit = {},
+    onCheckOutClicked: () -> Unit = {}
 ) {
-    val scrollableState = rememberScrollState()
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Content(
+            productDescription = productDescription,
+            productFlavors = productFlavors,
+            productNutritionState = productNutritionState,
+            productPreviewState = productPreviewState,
+        )
+        OrderActionBar(
+            state = orderState,
+            onAddItemClicked = onAddItemClicked,
+            onRemoveItemClicked = onRemoveItemClicked,
+            onCheckOutClicked = onCheckOutClicked,
+            modifier = Modifier
+                .navigationBarsPadding()
+                .padding(
+                    horizontal = 18.dp,
+                    vertical = 8.dp
+                )
+        )
+    }
+}
+
+@Composable
+private fun Content(
+    modifier: Modifier = Modifier,
+    productPreviewState: ProductPreviewState,
+    productFlavors: List<ProductFlavorState>,
+    productNutritionState: ProductNutritionState,
+    productDescription: String
+) {
+    val scrollState = rememberScrollState()
 
     Column(
-        modifier = modifier.verticalScroll(scrollableState)
+        modifier = modifier.verticalScroll(scrollState)
     ) {
         ProductPreviewSection(
             state = productPreviewState
+        )
+        Spacer(
+            modifier = Modifier.height(16.dp)
+        )
+        FlavorSection(
+            data = productFlavors,
+            modifier = Modifier.padding(horizontal = 18.dp)
+        )
+        Spacer(
+            modifier = Modifier.height(16.dp)
+        )
+        ProductNutritionSection(
+            state = productNutritionState,
+            modifier = Modifier.padding(horizontal = 18.dp)
+        )
+        Spacer(
+            modifier = Modifier.height(32.dp)
+        )
+        ProductDescriptionSection(
+            productDescription = productDescription,
+            modifier = Modifier
+                .navigationBarsPadding()
+                .padding(horizontal = 18.dp)
+                .padding(bottom = 128.dp)
         )
     }
 }
